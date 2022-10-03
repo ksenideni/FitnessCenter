3ff08544fb93215ef960601ad5bb52ffbe16033c
ksenideni <denishchenkovakseni@mail.ru>
2022-10-03 22:34:02 +0300

    javadoc for EventService

--- src/main/java/ru/ksenideni/fitnesscenter/events/EventService.java
+++ src/main/java/ru/ksenideni/fitnesscenter/events/EventService.java
@@ -2,7 +2,7 @@ package ru.ksenideni.fitnesscenter.events;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.ksenideni.fitnesscenter.training.Training;
+import ru.ksenideni.fitnesscenter.EventCreatingDTO;
 import ru.ksenideni.fitnesscenter.training.TrainingService;
 
 import java.util.List;
@@ -15,6 +15,10 @@ public class EventService {
     private final EventCreatingMapper eventCreatingMapper;
     private final EventMapper eventMapper;
 
+    /***
+     * Метод возвращает все события (тренировка и время)
+     * @return лист доступных событий
+     */
     public List<EventDTO> findAll() {
         List<EventDTO> allEvents = eventRepository.findAll().stream()
                 .map(eventMapper::fromEntity).toList();
@@ -22,12 +26,20 @@ public class EventService {
         return allEvents;
     }
 
+    /***
+     * Метод для удаления курса
+     * @param id - id курса, который нужно удалить
+     */
     public void deleteById(Long id) {
         eventRepository.deleteById(id);
     }
 
+    /***
+     * Метод для создания нового события
+     * @param event - событие, состоящее из тренировка и времени проведения (расписания)
+     */
     public void create(EventCreatingDTO event) {
-        Event savedEvent=eventCreatingMapper.fromDTO(event, trainingService.getByName(event.getTrainingName()));
+        Event savedEvent = eventCreatingMapper.fromDTO(event, trainingService.getByName(event.getTrainingName()));
         eventRepository.save(savedEvent);
     }
 }
